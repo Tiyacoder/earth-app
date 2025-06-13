087bdd500fb2486cfb3284ef39ea9d3b330b69f4
Tiyacoder <sarintanzil@gmail.com>
2025-06-13 23:54:35 +0530

    Initial commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import '../App.css';
+import './Hero.css';
 
 function Hero() {
   return (
     <section className="hero-section">
       <Container className="hero-content">
-        <h1 className="hero-title">E A R T H</h1>
+        <h1 className="hero-title">EARTH</h1>
         <p className="hero-subtitle">MULTIPURPOSE STORE</p>
       </Container>
     </section>
